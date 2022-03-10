8b6b262ef2507ac49b8986f75f90bf570cc06e08
sophiemassola <sophiemassola.sm@gmail.com>
2022-03-10 04:56:23 -0300

    Final

---
+++ index.js
@@ -0,0 +1,35 @@
+console.log('Entregable DOM');
+
+
+//Usuario decide si continúa con la lista predeterminada.
+let listado = prompt('Indique "si" si desea borrar los elementos predeterminados de la lista. De lo contrario, presione ENTER.').toUpperCase();
+if (listado == 'SI'){
+    supermercado.innerHTML = null;
+};
+
+//Definición de constantes y variables
+const productos = [];
+let a = ''
+
+
+//Creación del array que contiene los datos ingresados por el usuario.
+while (a != 'EXIT'){
+    a = prompt('Ingrese los productos de su lista de supermercado. Una vez finalizada, escriba EXIT.').toUpperCase();
+    
+    if (a != 'EXIT'){
+        productos.push(a);
+    }
+    
+
+}
+
+
+
+
+//Por cada elemento del array voy a crear un nuevo elemento y voy a hacer un append.
+productos.forEach( (x) => {
+    //console.log(x);
+    const nuevoProducto = document.createElement('li');
+    nuevoProducto.innerText = x;
+    supermercado.append(nuevoProducto);
+})
